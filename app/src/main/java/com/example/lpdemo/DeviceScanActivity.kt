12d2de76b1710e4454469557bab1c2091b23cd9d
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2024-10-12 12:45:17 +0800

    ahhh

--- app/src/main/java/com/example/lpdemo/DeviceScanActivity.kt
+++ app/src/main/java/com/example/lpdemo/DeviceScanActivity.kt
@@ -1,24 +1,66 @@
 package com.example.lpdemo
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
+import android.util.SparseArray
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.lpdemo.databinding.ActivityDeviceScanBinding
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.jeremyliao.liveeventbus.LiveEventBus
+import com.lepu.blepro.event.EventMsgConst
+import com.lepu.blepro.ext.BleServiceHelper
+import com.lepu.blepro.objs.Bluetooth
+import com.lepu.blepro.objs.BluetoothController
+import com.lepu.blepro.observer.BleChangeObserver
 
-class DeviceScanActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityDeviceScanBinding
+class DeviceScanActivity : AppCompatActivity(), BleChangeObserver {
+    val TAG: String = "DeviceScanActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityDeviceScanBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_device_scan)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        initView()
+        initService()
     }
 
+    private val models = intArrayOf(Bluetooth.MODEL_O2RING)
+    fun initService() {
+        if (BleServiceHelper.BleServiceHelper.checkService()) {
+            // BleService already init
+            BleServiceHelper.BleServiceHelper.startScan(models)
+        } else {
+            // Save the original file path. Er1, VBeat and HHM1 are currently supported
+            val rawFolders = SparseArray<String>()
+            rawFolders.set(Bluetooth.MODEL_ER1, "${getExternalFilesDir(null)?.absolutePath}/er1")
+            rawFolders.set(Bluetooth.MODEL_HHM1, "${getExternalFilesDir(null)?.absolutePath}/er1")
+            rawFolders.set(Bluetooth.MODEL_ER1S, "${getExternalFilesDir(null)?.absolutePath}/er1")
+            rawFolders.set(Bluetooth.MODEL_ER1_S, "${getExternalFilesDir(null)?.absolutePath}/er1")
+            rawFolders.set(Bluetooth.MODEL_ER1_H, "${getExternalFilesDir(null)?.absolutePath}/er1")
+            rawFolders.set(Bluetooth.MODEL_ER1_W, "${getExternalFilesDir(null)?.absolutePath}/er1")
+            rawFolders.set(Bluetooth.MODEL_ER1_L, "${getExternalFilesDir(null)?.absolutePath}/er1")
+
+            // initRawFolder必须在initService之前调用
+            BleServiceHelper.BleServiceHelper.initRawFolder(rawFolders).initService(application).initLog(true)
+        }
+    }
+
+    fun initView(){
+        LiveEventBus.get<Bluetooth>(EventMsgConst.Discovery.EventDeviceFound).observe(this) {
+            Log.d(TAG, "EventDeviceFound")
+            for (b in BluetoothController.getDevices()) {
+                Log.d(TAG, "\t - "+b.name + " | " + b.macAddr)
+            }
+        }
+    }
+
+    override fun onBleStateChanged(model: Int, state: Int) {
+        TODO("Not yet implemented")
+    }
 }
