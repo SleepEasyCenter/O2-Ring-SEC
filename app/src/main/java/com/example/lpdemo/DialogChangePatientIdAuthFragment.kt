b1f0bf24d13f621049b25fde350d1c2e323c1a9c
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2024-10-14 17:35:53 +0800

    AHHHHH

---
+++ app/src/main/java/com/example/lpdemo/DialogChangePatientIdAuthFragment.kt
@@ -0,0 +1,72 @@
+package com.example.lpdemo
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.lpdemo.databinding.FragmentSettingsBinding
+import com.example.lpdemo.databinding.FragmentSettingsChangePatientIdDialogBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DialogChangePatientIdAuthFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DialogChangePatientIdAuthFragment : DialogFragment() {
+    private var binding: FragmentSettingsChangePatientIdDialogBinding? = null
+
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            // Use the Builder class for convenient dialog construction.
+            val builder = AlertDialog.Builder(it)
+            builder.setMessage("Start game")
+                .setPositiveButton("Start") { dialog, id ->
+                    // START THE GAME!
+                }
+                .setNegativeButton("Cancel") { dialog, id ->
+                    // User cancelled the dialog.
+                }
+            // Create the AlertDialog object and return it.
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSettingsChangePatientIdDialogBinding.inflate(inflater, container, false);
+
+        return binding?.root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment SettingsChangePatientIdDialogFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            DialogChangePatientIdAuthFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
