601cd7f3f1ee1c3ab7befd3770a1f7d273804846
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2024-10-14 16:58:04 +0800

    able to receive pdf in app

--- app/src/main/java/com/example/lpdemo/ShareReceiveAndUploadActivity.kt
+++ app/src/main/java/com/example/lpdemo/ShareReceiveAndUploadActivity.kt
@@ -1,6 +1,10 @@
 package com.example.lpdemo
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Parcelable
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,12 +15,38 @@ class ShareReceiveAndUploadActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityShareReceiveAndUploadBinding;
 
+    val TAG: String = "ShareReceiveAndUploadActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityShareReceiveAndUploadBinding.inflate(layoutInflater);
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar);
         actionBar?.setDisplayHomeAsUpEnabled(true)
+        when{
+            intent?.action == Intent.ACTION_SEND -> {
+                if ("text/csv" == intent.type) {
+                    handleSendCSV(intent)
+                } else if (intent.type == "application/pdf") {
+                    handleSendPdf(intent)
+                }
+            }
+            else -> {
+
+            }
+        }
+
+    }
+    fun handleSendCSV(intent: Intent){
+        intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
+            // Update UI to reflect text being shared
+        }
+    }
+    fun handleSendPdf(intent: Intent){
+         (intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri).let { data ->
+             Log.d(TAG, "Received pdf:" + data.toString())
+
+         }
 
     }
 
